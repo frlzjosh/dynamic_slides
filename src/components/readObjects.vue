0749b8c22009b81706aea6fb48ddedf4f21eeea3
frlzjosh <joshua.magdaleno.472@my.csun.edu>
2019-04-30 13:49:17 -0700

    trying to figure out why my state won't update right away

--- src/components/readObjects.vue
+++ src/components/readObjects.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <p v-for="(a, key) in alc" :key="key">{{ a }}</p>
-    <button :click="decrement_slides()">back</button>
-    <button :click="increment_slides()">next</button>
+    <button v-on:click="decrement_slides()">back</button>
+    <button v-on:click="increment_slides()">next</button>
   </div>
 </template>
 <script>
@@ -12,20 +12,25 @@ export default {
   data() {
     return {
       alc: slides,
-      slide_number: 0 
+      slide_number: 0
     };
-  },
+  },  
   created() {},
   methods: {
     ...mapActions(["getSlideInfo"]),
     increment_slides: function() {
-      console.log(this.slide_number++); 
+      this.slide_number++
+      console.log(this.slide_number)
       return this.slide_number
     },  
     decrement_slides: function() {
-      this.slide_number = this.slide_number--
-      return this.slide_number;
+      this.slide_number--
+      console.log(this.slide_number)
+      return this.slide_number
     }
+  },
+  mounted(){
+    this.slide_number
   }
 };
 </script>
